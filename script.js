176b6f8ab0a2095886d1560874ca744046c85583
grazielasaibert <graziela.saibert.silva@escola.pr.gov.br>
2025-08-28 16:47:07 -0300

    .

--- script.js
+++ script.js
@@ -7,11 +7,11 @@ const texto = document.queryselector('#entrda-de-texto').value;
 const camporesultado =document.queryselector('#resultado-palavrachave');
 camporesultado.textcontent = texto 
 const palavras = processatexto(texto);
+
 camporesultado.textocontent = palavras.join("-")
 }
 
 function processatexto(texto){
     let palavras = texto.split(/[^a-za-z]+/);
     return palavras;
-
 }
