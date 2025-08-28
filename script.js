e73d3e43e2a4ee42ba284d85d965769497cb8ea2
grazielasaibert <graziela.saibert.silva@escola.pr.gov.br>
2025-08-28 16:44:31 -0300

    .

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
+
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+
 function mostraPalavrasChave() {
 const texto = document.queryselector('#entrda-de-texto').value; 
 const camporesultado =document.queryselector('#resultado-palavrachave');
 camporesultado.textcontent = texto 
-const palavras = texto.split("");
+const palavras = processatexto(texto);
 camporesultado.textocontent = palavras.join("-")
+}
+
+function processatexto(texto){
+    let palavras = texto.split(/[^a-za-z]+/);
+    return palavras;
+
 }
